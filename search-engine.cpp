300a1ecf5f053618ff1eb7ee00d975eb1701ca52
yshiroya <yshiroya@purdue.edu>
2015-07-21 21:33:01 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -16,13 +16,13 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
 	else if(dictionaryType == BinarySearchDictionaryType) _wordToURLList = new BinarySearchDictionary();
 	else _wordToURLList = NULL;
 	
-	printf("aa\n");
+	printf("1.\n");
 	
 	dt = dictionaryType;
   // Populate dictionary and sort it if necessary
   
   FILE *file = fopen("url.txt", "r");
-  
+  printf("2.\n");
   char * local_buffer = (char*) malloc(5000);
   strcpy(local_buffer, "");
   int maxURLs = 1001;
