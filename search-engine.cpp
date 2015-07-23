b6a29426d72a6c23143a85b004043b07fb822a4e
yshiroya <yshiroya@purdue.edu>
2015-07-23 04:35:29 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -341,7 +341,7 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
   
   if(url_list[i] == NULL) continue;
   
-    	fprintf( fout, "<h3>%d. <a href=\"%s\">%s</a><h3>\n", count2+1, url_list[i]->_url, url_list[i]->_url );
+    	fprintf( fout, "<h3>%d. <a href=\"%s\"><font face=\"Helvetica\">%s</font></a><h3>\n", count2+1, url_list[i]->_url, url_list[i]->_url );
     	fprintf( fout, "<font color=\"white\" font face=\"Helvetica\">%s</font><p>\n", url_list[i]->_description);
     	count2++;
     
