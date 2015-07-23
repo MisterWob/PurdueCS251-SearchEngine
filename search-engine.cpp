7bf6742ed14ad702bdb2d14497cdbe2da3acc628
yshiroya <yshiroya@purdue.edu>
2015-07-23 00:04:34 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -273,7 +273,7 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
 
   fprintf( fout, "<TITLE>Search Results</TITLE>\r\n");
   fprintf( fout, "<H1> <Center><em>Boiler Search</em></H1>\n");
-  fprintf( fout, "<H2> Search Results for \"%s\"</center></H2>\n", word_ext );
+  fprintf( fout, "<H2> Search Results for \"%s\"</center></H2>\n", cat_string );
 
   URLRecord ** url_list = new URLRecord * [500];
   int count1 = 0;
@@ -337,7 +337,7 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
   if(url_list[i] == NULL) continue;
   
     	fprintf( fout, "<h3>%d. <a href=\"%s\">%s</a><h3>\n", count2+1, url_list[i]->_url, url_list[i]->_url );
-    	fprintf( fout, "<i>%s<p></i>\n", url_list[i]->_description );
+    	fprintf( fout, "<i>%s<p></i>\n", url_list[i]->_description);
     	count2++;
     
   }
