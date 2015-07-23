5bdd422c4d48378eb82e2ec9808f771b2082662b
yshiroya <yshiroya@purdue.edu>
2015-07-23 03:33:34 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -178,9 +178,9 @@ SearchEngine::dispatch( FILE * fout, const char * documentRequested)
     fprintf(fout, "<H2>\n");
     fprintf(fout, "<font color=\"gray\" font size=\"5\" font face=\"Helvetica\">v1.0</font>");
     fprintf(fout, "<FORM ACTION=\"search\">\n");
-    fprintf(fout, "Search:\n");
+    fprintf(fout, "<font size=\"10\">Search:</font>\n");
     fprintf(fout, "<INPUT TYPE=\"text\" NAME=\"word\" MAXLENGTH=\"1000\"><P>\n");
-    fprintf(fout, "<p>&reg;<font size=\"5\">Yash Shiroya | 2015</font></p>");
+    fprintf(fout, "<p><font size=\"5\">&reg;Yash Shiroya | 2015</font></p>");
     fprintf(fout, "</H2>\n");
     fprintf(fout, "</FORM></CENTER>\n");
     return;
